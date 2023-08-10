c8dcae3301e4d6bfa1454216082ab6cfef02539b
Emmanuel <oladepogboyega@gmail.com>
2023-08-10 16:53:13 -0700

    My if else and Loop work

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Adds two integers and returns the result."""
+    result = a + b
+    return result
+
+if __name__ == "__main__":
+    num1 = int(input("Enter the first integer: "))
+    num2 = int(input("Enter the second integer: "))
+    sum_result = add(num1, num2)
+    print(f"The sum of {num1} and {num2} is {sum_result}.")
