6970e8872964f5c4d86ab1fa13eb5f355ec4fe89
Emmanuel <oladepogboyega@gmail.com>
2023-08-10 16:22:21 -0700

    My if else loop work

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 
 def islower(c):
-    return ord(c) >= ord('a') and ord(c) <= ord('z')
+    """Check if a character is lowercase."""
+    if ord('a') <= ord(c) <= ord('z'):
+        return True
+    return False
 
-# Test cases
-print(islower('a'))  # Output: True
-print(islower('Z'))  # Output: False
-print(islower('7'))  # Output: False
+if __name__ == "__main__":
+    char = input("Enter a character: ")
+    if len(char) == 1:
+        if islower(char):
+            print(f"{char} is lowercase.")
+        else:
+            print(f"{char} is not lowercase.")
+    else:
+        print("Please enter a single character.")
+
