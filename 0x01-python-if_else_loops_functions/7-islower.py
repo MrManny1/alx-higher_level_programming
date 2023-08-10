fba3ae19c7819bc1026e9b64b9071a6e8f3f33ab
Emmanuel <oladepogboyega@gmail.com>
2023-08-10 16:26:17 -0700

    My if else loop

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
+# 7-islower.py
 
 def islower(c):
-    """Check if a character is lowercase."""
-    if ord('a') <= ord(c) <= ord('z'):
+    """Check for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
-    return False
-
-if __name__ == "__main__":
-    char = input("Enter a character: ")
-    if len(char) == 1:
-        if islower(char):
-            print(f"{char} is lowercase.")
-        else:
-            print(f"{char} is not lowercase.")
     else:
-        print("Please enter a single character.")
+        return False
+
