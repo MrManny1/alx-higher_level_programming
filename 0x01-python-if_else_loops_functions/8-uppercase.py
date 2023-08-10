1289cae1b3bed42f4c8110783af48ec494d3e262
Emmanuel <oladepogboyega@gmail.com>
2023-08-10 16:45:07 -0700

    My if else and loop work

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
+# 8-uppercase.py
 
-def uppercase(s):
-    """Prints a string in uppercase followed by a new line."""
-    for char in s:
-        if ord('a') <= ord(char) <= ord('z'):
-            print(chr(ord(char) - 32), end='')
-        else:
-            print(char, end='')
-    print()
 
-if __name__ == "__main__":
-    input_string = input("Enter a string: ")
-    uppercase(input_string)
+def uppercase(str):
+    """Print a string in uppercase."""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
