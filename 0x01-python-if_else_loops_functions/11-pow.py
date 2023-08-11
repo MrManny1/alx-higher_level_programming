bdee74896721e526b0ca0d1b292dad1f6ebf6e5b
Emmanuel <oladepogboyega@gmail.com>
2023-08-10 17:05:39 -0700

    My if else and loop work

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
+# 11-pow.py
+
 
 def pow(a, b):
-    """Computes a to the power of b and returns the result."""
-    result = 1
-    for _ in range(b):
-        result *= a
-    return result
-
-if __name__ == "__main__":
-    base = int(input("Enter the base: "))
-    exponent = int(input("Enter the exponent: "))
-    power_result = pow(base, exponent)
-    print(f"{base} raised to the power of {exponent} is {power_result}.")
+    """Return a to the power of b."""
+    return (a ** b)
