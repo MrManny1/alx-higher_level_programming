6dbbabf2a29b58a4566f3093836e93f0ba1f992f
Emmanuel <oladepogboyega@gmail.com>
2023-08-28 11:11:20 -0700

    My python Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,23 +1,23 @@
+0-safe_print_list.py
+
 #!/usr/bin/python3
 
-def unique_safe_print_list(my_list=[], x=0):
-    """Print up to x elements of a list, handling errors gracefully.
+def safe_print_list(my_list=[], x=0):
+    """Print x elememts of a list.
 
     Args:
         my_list (list): The list to print elements from.
-        x (int): The maximum number of elements from my_list to print.
+        x (int): The number of elements of my_list to print.
 
     Returns:
-        The actual number of elements successfully printed.
+        The number of elements printed.
     """
-    num_printed = 0
+    ret = 0
     for i in range(x):
         try:
-            print("Item: {}".format(my_list[i]), end="")
-            num_printed += 1
+            print("{}".format(my_list[i]), end="")
+            ret += 1
         except IndexError:
-            print("\nReached the end of the list.")
             break
-    print("\nPrinted {} elements.".format(num_printed))
-    return num_printed
-
+    print("")
+    return (ret)
