a180fca494b162edebc2713ac67dceb82103c5db
Emmanuel <oladepogboyega@gmail.com>
2023-09-12 12:23:57 -0700

    My Python Inheritance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,9 +5,7 @@
 class BaseGeometry:
     """Reprsent base geometry."""
 
-
     def area(self):
-
         """Not yet implemented."""
         raise Exception("area() is not implemented")
 
@@ -15,11 +13,11 @@ class BaseGeometry:
         """Validate a parameter as an integer.
 
         Args:
-        name (str): The name of the parameter.
-        value (int): The parameter to validate.
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
         Raises:
-        TypeError: If value is not an integer.
-        ValueError: If value is <= 0.
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
         """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
