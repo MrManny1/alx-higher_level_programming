f18ac8897833d45724ff11a4d2e29cd6570295ef
Emmanuel <oladepogboyega@gmail.com>
2023-12-01 02:59:41 -0800

    My Python Network header post email and error code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
-"""Python script that 
-- fetches https://alx-intranet.hbtn.io/status
-- use the package urllib
 """A script that
 - fetches https://alx-intranet.hbtn.io/status.
 - uses urlib package
 """
 
-if __name__ == "__main__":
 
 if __name__ == '__main__':
     import urllib.request
-    
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        html = res.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
